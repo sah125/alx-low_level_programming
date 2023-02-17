9aa7e219785c13bcdb34c9d56fce79e7342cb6c0
sah125 <sah.sakhile@gmail.com>
2023-02-17 07:13:53 -0800

    Task 4Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main : A program that print line withot printf()
+ * main - A program that print line withot printf()
  * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
