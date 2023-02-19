bb09a10615b7ca4a5c51150daa390d139e235618
sah125 <sah.sakhile@gmail.com>
2023-02-19 05:35:12 -0800

    mama

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/**
+/*
  * main - A program assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.
  * Return: Always 0 (Success).
  */
