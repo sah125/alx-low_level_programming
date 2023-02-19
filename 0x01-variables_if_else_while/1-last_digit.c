90988d132cd5699b5cde1c56fd5d9e4b5e91df86
sah125 <sah.sakhile@gmail.com>
2023-02-19 04:49:58 -0800

    wow

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("%d is %d and is greater than 5", n, n % 10);
-	
+	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{ 
 		printf("%d is %d and is less than 6 and not 0", n, n % 10);
