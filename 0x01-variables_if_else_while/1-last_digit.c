7b3809ac458c9e960fe2c1ec8a9f3eba2d1d607f
sah125 <sah.sakhile@gmail.com>
2023-02-20 15:12:37 -0800

    safe4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - A program assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.
+ * main - A program assign a random .
  * Return: Always 0 (Success).
  */
 int main(void)
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - a program that display Letters not q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i < 'z' ; i++)
+		if (i != e || i != q)
+			putchar(i);
+	return (0);
+}
