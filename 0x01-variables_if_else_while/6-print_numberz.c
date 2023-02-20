e83fe35fc7dc139664b41b69b8a55c7cc2d65aed
sah125 <sah.sakhile@gmail.com>
2023-02-20 15:52:57 -0800

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - A Program that displays unit Numbers
+ * Return: Alzways 0 Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf(i);
+	printf("\n");
+	return (0);
+
+}
