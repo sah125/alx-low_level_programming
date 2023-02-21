9a0370b3bb9fa574788c9249144049f442229d60
sah125 <sah.sakhile@gmail.com>
2023-02-20 16:43:57 -0800

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,12 @@
 int main(void)
 {
 	char i;
+	char j;
+
 	for (i = 0 ; i < 10 ; i++)
-	putchar(i + '0');
-for (i = 'a' ; i <= 'f' ; i++)
-putchar(i);
-putchar('\n');
-return (0);
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
