e00bc5b6e5b68c1c5ddebaff16258e82b132151c
sah125 <sah.sakhile@gmail.com>
2023-02-21 04:52:08 -0800

    Task 1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main function 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			for (k = 0 ; k < 10 ; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					if (i && j < k && l )
+					{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i + j + k + l != 35)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
