706a9b3c5dd8d4aae03014119c2ead72e49ffcd9
sah125 <sah.sakhile@gmail.com>
2023-02-24 13:13:23 -0800

    Update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - Tester 
- * Return 
+ * main - Tester functioni 
+ * Return: 0 
  */
-
 int main(void)
 {
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
 	return (0);
