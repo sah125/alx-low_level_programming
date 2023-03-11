de66d381060534497cd8b518ee3e528698cc3047
sah125 <sah.sakhile@gmail.com>
2023-03-11 14:45:44 -0800

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-void _puts(char *str)
 #include<stdio.h>
 /**
  * main - Entry point
@@ -11,4 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
