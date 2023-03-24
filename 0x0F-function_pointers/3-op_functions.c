3dce1a06d8c25d7e34cf11150258a40b4c7cbee0
sah125 <sah.sakhile@gmail.com>
2023-03-24 15:13:15 -0700

    Update for function pointer

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
+		i++;
+	}
+
+	return (0);
+}
